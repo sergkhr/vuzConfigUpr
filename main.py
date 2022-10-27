9bd7a5305499ef203f382aa442e7c94006ad9a84
sergkhr <46621907+sergkhr@users.noreply.github.com>
2022-10-27 12:01:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import json
 with open("civgraph.json", "r") as f:
     civ = json.load(f)
     with open("Makefile", "w") as fout:
+        fout.write(".PHONY: clean\n")
         for key in civ.keys():
             fout.write(key + ': ' + ' '.join(civ[key]) + "\n\t@echo \"" + key + "\"\n" + "\t@echo . > " + key +\
                        "\n")
+        fout.write("clean: \n\t@del ")
+        for key in civ.keys():
+            fout.write(key + ' ')
